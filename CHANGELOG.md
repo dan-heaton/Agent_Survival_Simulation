160c8d76ed01bbdd8ff6b4defedc4619430421b9
Dan Heaton <dheaton815@gmail.com>
2021-01-16 05:28:10 +0000

    Added intialisation of environ tiles w/ energy sources + obstacles, added visualisation function of environment tiles, and moved code to 'src' subdirectory

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.1.2 - 16/01/21
+- Added initialisation of environment tiles, along with addition of energy sources and obstacles
+- Added visualisation function of environment tiles
+- Moved stuff to 'src' subdirectory
+
 ## 0.1.1 - 15/01/21
 - Added to-do list
 - Changed repo name and local project name from 'CPP-Project' to 'Agent_Survival_Simulation'
--- src/basic_simulation.cpp
+++ src/basic_simulation.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include<cstdlib>
-#include<ctime>
-#include<list>
+#include <cstdlib>
+#include <ctime>
+#include <list>
+#include <vector>
 
 using namespace std;
 
 const int MAX_ENERGY_VAL = 100;
-const int X_BOUND = 100;
-const int Y_BOUND = 100;
+const int X_BOUND = 15;
+const int Y_BOUND = 15;
 
 
 struct Obstacle {
@@ -61,29 +62,45 @@ class BasicEnvironment {
     private:
         list <Obstacle> obstacles;
         list <Energy> energy_sources;
+        vector <vector <char>> environment_tiles;
         int x_bound;
         int y_bound;
 
-    public:
-        BasicEnvironment(int x_bound, int y_bound, int num_obstacles, int num_energy_sources) 
-                        :x_bound(x_bound), y_bound(y_bound) {
-            
-            // Creates 'num_obstacles' objects at random positions within the environment bounds
+        void insert_obstacles_energy(int num_obstacles, int num_energy_sources) {
+            // Creates 'num_obstacles' objects at random positions within the environment bounds, and inserts them into the 2D board
             for (int i=0; i<num_obstacles; i++) {
                 int x_pos = rand() % x_bound + 1;
                 int y_pos = rand() % y_bound + 1;
                 Obstacle obst(x_pos, y_pos);
                 obstacles.push_back(obst);
+                environment_tiles[y_pos-1][x_pos-1] = 'O';
             }
 
-            // Creates 'num_energy_sources' objects at random positions within the environment bounds
+            // Creates 'num_energy_sources' objects at random positions within the environment bounds, and inserts them into the 2D board
             for (int i=0; i<num_energy_sources; i++) {
                 int x_pos = rand() % x_bound + 1;
                 int y_pos = rand() % y_bound + 1;
                 int energy_val = rand() % MAX_ENERGY_VAL + 1;
                 Energy ener(x_pos, y_pos, energy_val);
                 energy_sources.push_back(ener);
+                environment_tiles[y_pos-1][x_pos-1] = 'E';
             }
+        }
+
+    public:
+        BasicEnvironment(int x_bound, int y_bound, int num_obstacles, int num_energy_sources) 
+                        :x_bound(x_bound), y_bound(y_bound) {
+
+            // Initalises all environment tiles as empty spaces ("-") based on supplied dimensions, 
+            // before obstacles or energy sources are added
+            for (int i=0; i<y_bound; i++) {
+                vector <char> environment_tiles_row;
+                for (int j=0; j<x_bound; j++) {
+                    environment_tiles_row.push_back('-');
+                }
+                environment_tiles.push_back(environment_tiles_row);
+            }
+            insert_obstacles_energy(num_obstacles, num_energy_sources);
         }
 
         void state_objects() {
@@ -95,8 +112,13 @@ class BasicEnvironment {
             }
         }
 
-        void visualise(){
-
+        void environment_visualise(){
+            for (int i=0; i<environment_tiles.size(); i++) {
+                for (int j=0; j<environment_tiles[i].size(); j++) {
+                    cout << " " << environment_tiles[i][j] << " ";
+                }
+                cout << endl;
+            }
         }
 
 };
@@ -109,10 +131,11 @@ int main() {
     srand(time(NULL));
 
     BasicAgent agent_1("elo", X_BOUND, Y_BOUND);
-    agent_1.move_x(5);
+    agent_1.move_x(1);
     BasicAgent agent_2("ahoy-hoy", X_BOUND, Y_BOUND);
-    agent_2.move_y(10);
+    agent_2.move_y(1);
 
-    BasicEnvironment environment(X_BOUND, Y_BOUND, 3, 5);
+    BasicEnvironment environment(X_BOUND, Y_BOUND, 2, 3);
     environment.state_objects();
+    environment.environment_visualise();
 }
